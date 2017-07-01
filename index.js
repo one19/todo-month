02e57ffc1f42d314d5c906e09e0235215048df8c
Drew Showalter <maelstroswim@gmail.com>
2017-07-01 22:33:48 +1000

    :recycle: fix up linting.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-const colors = require('colors');
+const colors = require('colors'); // eslint-disable-line no-unused-vars
+// this ~crazy-scary~ _amazing_ package mutates Strings globally
 const copyPaster = require('copy-paste');
 
 const supposedDate = process.argv[2];
@@ -9,29 +10,28 @@ if (!date.getDate()) date = new Date();
 const MONTH_NAMES = [
   'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
   'September', 'October', 'November', 'December'
-];  
+];
 const DAY_NAMES = [
   'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 ];
 
 const year = date.getFullYear();
 const month = date.getMonth();
-const day = date.getDate();
 
 const weekday = (year, month, day) => new Date(year, month, day).getDay();
 const daysInMonth = (year, month) =>
   new Date(year, month, 0).getDate();
-const dayAbbrevAppender = (dayNumber) =>
+const dayAbbrevAppender = dayNumber =>
   dayNumber === 0 || dayNumber === 6 ? DAY_NAMES[dayNumber].toUpperCase() :
     DAY_NAMES[dayNumber].slice(0, 2);
 
 const text = [...Array(daysInMonth(year, month + 1))].reduce((text, _, index) =>
   text.concat(
-    `### ${('0' + (index + 1)).slice(-2)}-${('0' + month).slice(-2)}-${year}`,
+    `### ${(`0${index + 1}`).slice(-2)}-${(`0${month}`).slice(-2)}-${year}`,
     ` - ${dayAbbrevAppender(weekday(year, month, index + 1))}`,
     '\n- \n'
   )
   , `NIGHTLY ${MONTH_NAMES[month].toUpperCase()}\n`);
 
 copyPaster.copy(text.concat('\n\n#todo'));
-console.log('Wrote your month to the clipboard!'.rainbow)
+console.log('Wrote your month to the clipboard!'.rainbow);
